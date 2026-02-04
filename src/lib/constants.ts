01b376c723eb282f02ff7ef09fe27b6d0f05190d
Niccolo Di Maria <ndimaria3311@icloud.com>
2026-02-03 19:29:45 -0500

    Revert "Migrate site to React + Tailwind premium UI"
    This reverts commit 9196821929dca9f9bb4b6f871cfb845676e3e1ed.

--- src/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const SUPABASE_URL = "https://vnzlaidwxgiaowhxtuvi.supabase.co"
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuemxhaWR3eGdpYW93aHh0dXZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4MjcwNzksImV4cCI6MjA4NDQwMzA3OX0.AKiGX3xbgMKtUFYxHKEaAKwacFqmjPEsy56ofU1eDQQ"
-export const FN_CREATE_CHECKOUT = "create-checkout-session"
-export const FN_PORTAL = "create-portal-session"
